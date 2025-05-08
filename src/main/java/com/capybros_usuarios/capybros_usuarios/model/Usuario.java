2e2acfd6d00ae052fcf2f01fa8a8b6a565729409
logicaltomy <logicaltomy@gmail.com>
2025-05-08 14:20:06 -0400

    Version 0.1 Usuarios

---
+++ src/main/java/com/capybros_usuarios/capybros_usuarios/model/Usuario.java
@@ -0,0 +1,18 @@
+package com.capybros_usuarios.capybros_usuarios.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    
+}
