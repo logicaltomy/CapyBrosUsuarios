08126a5f9eda84e25badbbcff0f69cfb572d0f2e
logicaltomy <logicaltomy@gmail.com>
2025-05-08 16:16:43 -0400

    Version 0.1 Usuarios

--- src/main/java/com/capybros_usuarios/capybros_usuarios/model/Usuario.java
+++ src/main/java/com/capybros_usuarios/capybros_usuarios/model/Usuario.java
@@ -5,14 +5,45 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
+// Este paquete contiene todas las clases que van a estar en la BD
 @Entity
-@Table(name = "Usuario")
+@Table(name = "usuarios")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Usuario {
+    // La ID será auto-incrementable y la PK
+    // Adding to this; the @Id indicates that it's the PK
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+
+    // El nombre no podrá ser nulo en la BD
+    @Column(nullable = false)
+    private String nombreUsuario;
+
+    @Column(nullable = false)
+    private String contrasena;
+
+    private Boolean estado = true;
+
+    // El atributo fechaCreacion será autogenerado
+    // Y cada vez que se crea un nuevo usuario
+    // Se le asignará la fecha actual
+    @Column(nullable = false)
+    private LocalDateTime fechaCreacion = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private List<Permiso> permisos;
+
+
+
+
+
     
+
+
 }
