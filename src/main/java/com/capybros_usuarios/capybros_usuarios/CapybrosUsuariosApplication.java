2e65efc4d4be8f29bd623275ef7431303ac9eaed
logicaltomy <logicaltomy@gmail.com>
2025-05-08 14:10:06 -0400

    Version 0.0 Usuarios

---
+++ src/main/java/com/capybros_usuarios/capybros_usuarios/CapybrosUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.capybros_usuarios.capybros_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapybrosUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapybrosUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capybros_usuarios/capybros_usuarios/CapybrosUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capybros_usuarios.capybros_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapybrosUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
